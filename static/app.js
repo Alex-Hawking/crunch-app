f5f6a807f046b3031305c345a4b851216a4a1474
Alex Hawking <44057725+Handmade-Studios@users.noreply.github.com>
2020-11-23 16:40:20 +0800

    added colours to verification

--- static/app.js
+++ static/app.js
@@ -2,14 +2,17 @@ var socket = io();
 
 $('#codeInput').keyup(() => {
     if ($('#codeInput').val().length == 4) {
-        socket.emit('check-room', $('#codeInput').val(), function(valid) {
-            if (valid) {
-                $('#codeValidator').text('Code Is Valid')
+        socket.emit('check-room', $('#codeInput').val(), function(codeIsValid) {
+            if (codeIsValid) {
+                $('#codeValidator').text('Code Is Valid');
+                $('#codeValidator').css('color', 'green');
             } else {
-                $('#codeValidator').text('Please Enter A Valid Code')
+                $('#codeValidator').text('Please Enter A Valid Code');
+                $('#codeValidator').css('color', 'red');
             }
         })
     } else {
         $('#codeValidator').text('Please Enter A Valid Code');
+        $('#codeValidator').css('color', 'red');
     }
 })
